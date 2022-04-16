15e86ca6b34be5a963a424c92f87f97bf71bb694
Keigo Habara <k.habara.aa@gmail.com>
2022-04-16 20:27:11 +0900

    add nightly code and CI

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,26 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+// https://github.com/konstin/complex-manylinux-maturin-docker/blob/main/nightly-only/src/lib.rs
+
+#![feature(box_syntax)] // Make this crate nightly only
+
+use pyo3::prelude::*;
+
+#[pyclass]
+struct DummyClass {}
+
+#[pymethods]
+impl DummyClass {
+    #[staticmethod]
+    fn get_42() -> PyResult<usize> {
+        Ok(42)
     }
+}
+
+#[pymodule]
+fn maturin_example(_py: Python, m: &PyModule) -> PyResult<()> {
+    let _five = box 5;
+
+    m.add_class::<DummyClass>()?;
+    m.add("fourtytwo", 42)?;
+
+    Ok(())
 }
